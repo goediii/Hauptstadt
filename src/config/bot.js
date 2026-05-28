5868fa0ceec9a9708c4285ed6a04e5d29a943752
goediii <goedi254@gmail.com>
2026-05-28 23:56:54 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: ".gg/hptst,
+        name: ".gg/hptst",
         // Activity type number (0 = Playing).
         type: 1, 
       },
